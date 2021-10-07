d0d96dbe743f2908d5bade8ab4f09895e40cd8f9
Alex Alves <alexalvesfd@hotmail.com>
2021-10-07 12:22:23 -0300

    Projeto criado

---
+++ src/main/java/com/shopgamer/ShopgamerApplication.java
@@ -0,0 +1,13 @@
+package com.shopgamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopgamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopgamerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopgamer/ShopgamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopgamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopgamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
