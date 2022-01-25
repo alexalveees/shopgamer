c1114db167715be6db4d8cd11221bb0a6824e6a9
Alex Alves <alexalvesfd@hotmail.com>
2022-01-25 11:11:34 -0300

    Implementando Authorization e Authetication

--- src/main/java/com/shopgamer/config/SecurityConfig.java
+++ src/main/java/com/shopgamer/config/SecurityConfig.java
@@ -19,6 +19,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.shopgamer.security.JWTAuthenticationFilter;
+import com.shopgamer.security.JWTAuthorizationFilter;
+import com.shopgamer.security.JWTUtil;
+
 
 
 @Configuration
@@ -32,8 +36,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
     private Environment env;
 	
-	//@Autowired
-	//private JWTUtil jwtUtil;
+	@Autowired
+	private JWTUtil jwtUtil;
 	
 	private static final String[] PUBLIC_MATCHERS = {
 			"/h2-console/**"
@@ -63,8 +67,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
-		//http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-		//http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
